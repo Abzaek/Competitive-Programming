b641d5aa87ec8a5199ed81042b368e879fa08bc7
Abzaek <115345290+Abzaek@users.noreply.github.com>
2023-11-29 19:14:59 +0300

    Added solution for Convert the Temperature

---
+++ leetcode/convert-the-temperature.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convertTemperature(self, Celsius: float) -> List[float]:
+        
+        ans = [Celsius + 273.15,Celsius * 1.80 + 32.00]
+
+        return ans
+
+        
+
+        
+        
+
+        
