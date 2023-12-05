05dd2101294fc738cf7d03e99b4cedcb1ba6e837
Abzaek <115345290+Abzaek@users.noreply.github.com>
2023-12-05 16:02:27 +0300

    Added solution for Escape The Ghosts

--- leetcode/escape-the-ghosts.py
+++ leetcode/escape-the-ghosts.py
@@ -1,21 +1,13 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        if target in ghosts:
-            return False
-
-
+        
         for i in ghosts:
             if self.distance(i, target) <= self.distance([0, 0], target):
                 return False
         return True
-        
+
 
     def distance(self, l, t):
-        x1, y1 = l
-        x2, y2 = t
-
-        dx = abs(x1 - x2)
-        dy = abs(y1 - y2)
-
-        return dx + dy
+      
+        return abs(l[0] - t[0]) + abs(l[1] - t[1])
         
