313472a626139c4de911607ea40acfc44fe11a23
Abdulazez Zeinu Ali <abdulazez.zeinu@aait.edu.et>
2024-02-08 16:59:15 +0300

    Add solution for Sum of Absolute Differences in a Sorted Array

--- leetcode/leetcode/sum-of-absolute-differences-in-a-sorted-array.py
+++ leetcode/leetcode/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,21 +1,14 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        left_prefix = [0]*len(nums)
-        right_prefix = [0]*len(nums)
         ans = [0]*len(nums)
+        left = 0
+        right = 0
+        
+        for i in range(len(nums)):
+            ans[i] += nums[i]*i - left
+            left += nums[i]
 
-        for i in range(len(nums)):
-            left_prefix[i] += left_prefix[i - 1] + nums[i]
-            right_prefix[len(nums) - 1 - i] += right_prefix[(len(nums) - i)%len(nums)] + nums[len(nums) - 1 - i] 
-       
-        for i in range(len(nums)):
-            if i == 0:
-                ans[i] = right_prefix[i + 1] - nums[i]*(len(nums) - 1 - i)
-                continue
-            elif i == len(nums) - 1:
-                ans[i] = nums[i]*i - left_prefix[i - 1]
-                continue
-            ans[i] += right_prefix[i + 1] - nums[i]*(len(nums) - 1 - i)
-            ans[i] += nums[i]*i - left_prefix[i - 1]
+            ans[len(nums) - 1 - i] += right - nums[len(nums) - 1 -i]*i
+            right += nums[len(nums) - 1 - i]
         return ans
             
