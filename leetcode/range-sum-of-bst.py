b54274df03c82a91e9c1d8e4ee373a0c6d91541a
Abdulazez Zeinu Ali <abdulazez.zeinu@aait.edu.et>
2024-02-23 17:57:23 +0300

    Add solution for Range Sum of BST

---
+++ leetcode/range-sum-of-bst.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        def _sum(r):
+            if not r:
+                return 0
+            
+            if r.val >= low and r.val <= high:
+                return r.val + _sum(r.right) + _sum(r.left)
+            else:
+                return _sum(r.right) + _sum(r.left)
+                
+        return _sum(root)
