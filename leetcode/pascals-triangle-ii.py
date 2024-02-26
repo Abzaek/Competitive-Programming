63d207f7abaee0d5d65615760eff118dd03f0fdc
Abdulazez Zeinu Ali <abdulazez.zeinu@aait.edu.et>
2024-02-26 11:40:32 +0300

    Add solution for Pascal's Triangle II

--- leetcode/pascals-triangle-ii.py
+++ leetcode/pascals-triangle-ii.py
@@ -1,9 +1,31 @@
 class Solution:
-  def getRow(self, rowIndex: int) -> List[int]:
-    ans = [1] * (rowIndex + 1)
+    def __init__(self):
+        self.dict = {}
+    def getRow(self, rowIndex: int) -> List[int]:
+        arr = []
+        def pascal(row, index):
+            nonlocal arr
+            curr  = 0
+            for i in range(index):
+                acc = recc(row - 1, i)
+                arr.append(curr + acc)
+                curr = acc
+        def recc(row, index):
+            if not row or not index or row == index:
+                return 1
+            
+            if (row, index) not in self.dict:
+                x = recc(row - 1, index - 1) 
+                y = recc(row - 1, index)
+                self.dict[(row, index)] = x + y
+                return x + y
+            else:
+                return self.dict[(row, index)]
 
-    for i in range(2, rowIndex + 1):
-      for j in range(1, i):
-        ans[i - j] += ans[i - j - 1]
-
-    return ans
+        pascal(rowIndex, rowIndex + 1)
+        if arr:
+            arr[-1] = 1
+        return arr
+        
+        
+        
